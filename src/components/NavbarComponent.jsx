50bd5c34a0109a648acab76c3c8a182f7a1dc535
FadelYunus07 <fadelyunus07@gmail.com>
2023-11-17 19:59:42 +0700

    update project

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,18 +1,60 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { navLinks } from "../data/links";
 
 const NavbarComponent = () => {
+  const [changeColor, setChangeColor] = useState(false);
+  const changeBackgroundColor = () => {
+    if (window.scrollY > 10) {
+      setChangeColor(true);
+    } else {
+      setChangeColor(false);
+    }
+  };
+  useEffect(() => {
+    changeBackgroundColor();
+    window.addEventListener("scroll", changeBackgroundColor);
+
+    let eyeBall = document.querySelector(".eyeball"),
+      pupil = document.querySelector(".pupil"),
+      eyeArea = eyeBall.getBoundingClientRect(),
+      pupilArea = pupil.getBoundingClientRect(),
+      R = eyeArea.width / 2,
+      r = pupilArea.width / 2,
+      centerX = eyeArea.left + R,
+      centerY = eyeArea.top + R;
+
+    document.addEventListener("mousemove", (e) => {
+      let x = e.clientX - centerX,
+        y = e.clientY - centerY,
+        theta = Math.atan2(y, x),
+        angle = (theta * 180) / Math.PI + 360;
+
+      pupil.style.transform = `translateX(${R - r + "px"}) rotate(${angle + "deg"})`;
+      pupil.style.transformOrigin = `${r + "px"} center`;
+    });
+  }, []); // Menggunakan array kosong untuk memastikan hanya dijalankan sekali saat komponen dimuat
+
   return (
     <div>
-      <Navbar expand="lg d-flex">
+      <Navbar fixed="top" expand="lg d-flex" className={changeColor ? "color-active" : ""}>
         <Container>
           <Navbar.Brand className="fw-bold" href="#home">
             Curyculum Vitae.
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <div>
+            {/* Animasi Mata */}
+            <svg width="30" height="30" class="eye">
+              <circle cx="15" cy="15" r="15" class="eyeball" />
+              <circle cx="15" cy="15" r="8" class="pupil" />
+            </svg>
+            {/* Akhir Animasi Mata */}
+          </div>
+
+          <Navbar.Toggle className={changeColor ? "color-active" : ""} aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mx-auto text-center">
+            <Nav className="mx-auto text-end b-solid">
               {navLinks.map((link) => {
                 return (
                   <div className="nav-link fw-bold" key={link.id}>
@@ -23,18 +65,7 @@ const NavbarComponent = () => {
                 );
               })}
             </Nav>
-            <div className="text-center">
-              <button className="btn btn-outline-primary rounded-2">Join With Us</button>
-            </div>
           </Navbar.Collapse>
-          <div>
-            {/* Animasi Mata */}
-            <svg width="30" height="30" class="eye">
-              <circle cx="15" cy="15" r="15" class="eyeball" />
-              <circle cx="15" cy="15" r="8" class="pupil" />
-            </svg>
-            {/* Akhir Animasi Mata */}
-          </div>
         </Container>
       </Navbar>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,80 @@
 import { Container, Row, Col } from "react-bootstrap";
+import Image1 from "../image/fadelcrop.png";
+import TutWuri from "../image/TutWuri.png";
+import Smuhero from "../image/smuhero.png";
+import Unpam from "../image/unpam.png";
 
 const HomePage = () => {
   return (
-    <div className="homepage">
-      <header className="w-100 min-vh-100 d-flex align-items-center">
-        <Container>
-          <Row className="header-box d-flex align-items-center">
-            <Col>
-              <h1>CV Saya Bagus</h1>
-            </Col>
-          </Row>
-        </Container>
-      </header>
-    </div>
+    <>
+      <div className="homepage">
+        <header className="w-100 min-vh-100 d-flex align-items-center">
+          <Container>
+            <Row className="header-box d-flex align-items-center">
+              <Col className="text-center m-4">
+                <img src={Image1} alt="fadel1" />
+              </Col>
+              <Col className="text-center">
+                <p>Hello, Im</p>
+                <h1>Fadel Yunus Mahrus</h1>
+                <button>Download CV</button>
+                <button>Contact Me</button>
+                <div>
+                  <a className="gh" href="https://github.com/FadelYunus07">
+                    <i className="m-2 fs-5 fa-brands fa-github"></i>
+                  </a>
+                  <a className="ig" href="https://instagram.com/fadel.yunus07">
+                    <i className="m-2 fs-5 fa-brands fa-instagram"></i>
+                  </a>
+                  <a className="wa" href="https://tiktok.com">
+                    <i className="m-2 fs-5 fa-brands fa-whatsapp"></i>
+                  </a>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </header>
+        <div className="school w-100 min-vh-100">
+          <Container>
+            <Row className=" d-flex align-items-center text-center">
+              <div className="sd">
+                <img src={TutWuri} alt="sd" />
+                <h3>SD Negeri 1 Pengatigan</h3>
+                <h4>2006 - 2012</h4>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi minus quia dolores non dolor iure id doloremque suscipit eveniet iste. Asperiores soluta eaque illo minima animi illum doloremque eveniet eligendi quo facere
+                  voluptates exercitationem excepturi aliquid, mollitia quae? Odio, fuga?
+                </p>
+              </div>
+            </Row>
+            <Row className=" d-flex align-items-center text-center">
+              <Col>
+                <div className="sd">
+                  <img src={Smuhero} alt="smk" />
+                  <h3>SMK Muhammadiyah 6 Rogojampi</h3>
+                  <h4>2015 - 2018</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt suscipit cupiditate assumenda dignissimos nostrum illo nobis eaque doloremque commodi sint molestias dicta fugit nemo mollitia quia neque repellat
+                    exercitationem, repudiandae officiis fuga. Reprehenderit illo voluptas temporibus placeat nesciunt? Quas, veniam.
+                  </p>
+                </div>
+              </Col>
+              <Col>
+                <div className="sd">
+                  <img src={Unpam} alt="unpam" />
+                  <h3>Universitas Pamulang</h3>
+                  <h4>2019-2023</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste autem reprehenderit quis hic eos explicabo recusandae libero aspernatur? Expedita neque sunt quia iste odio labore at debitis vel ducimus provident. Quo saepe
+                    placeat ducimus aliquam qui. Qui quas unde eligendi.
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+    </>
   );
 };
 
